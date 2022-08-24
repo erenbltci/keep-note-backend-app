efd3d72484e023b2ae2e429144eb041989d86c9f
technoeren13 <erenbaltaci7878@gmail.com>
2022-08-24 15:09:49 +0300

    prism

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,30 +1,11 @@
 import fastify from "fastify";
-import connectDB, { prisma } from "./helpers/connectMongoDB";
 const app = fastify()
 
 require('dotenv').config()
 
 
-app.get('/', async function (request, reply) {
-
-  const newUser = await prisma.text.create({
-    data: {
-      text: "",
-      addedDate: new Date()
-
-    },
-  })
-
-  const text = await prisma.text.count()  
-    reply.send({ allText: text })
-  })
-
-
-
-
 app.listen({ port: 3000 }, (error, adress) => {
     if(error) throw error
-    connectDB()
     console.log(`Project running on ${adress}`)
 
 })
