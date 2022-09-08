e616fd654a113d1322b96f26f6a4f8a7f5ddc0a2
technoeren13 <erenbaltaci7878@gmail.com>
2022-09-08 22:10:41 +0300

    some changes

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -2,10 +2,6 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 
-import Navbar from './components/NavBar';
-import HomePage from './pages/Home';
-import NotesPage from './pages/MyNotes';
-
 function App() {
   return (
     <>
